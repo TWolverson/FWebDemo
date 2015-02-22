b14d3ba214b85610312935debc9ff08e5ca2878d
TWolverson <t_wolverson@hotmail.com>
2015-02-22 17:53:26 +0000

    Initial project skeleton

---
+++ api/Global.fs
@@ -0,0 +1,24 @@
+﻿namespace api
+
+open System
+open System.Web.Http
+open System.Net
+open System.Net.Http
+ 
+type HttpRouteDefaults = { Controller : string; Id : obj }
+ 
+ type ValuesController() =
+    inherit ApiController()
+    member this.Get() = 
+        this.Request.CreateResponse(
+            HttpStatusCode.OK,
+            "{ \"response\" : \"Hello\" }")
+
+type Global() =
+    inherit System.Web.HttpApplication()
+    member this.Application_Start (sender : obj) (e : EventArgs) =
+        (GlobalConfiguration.Configuration.Routes.MapHttpRoute(
+            "DefaultAPI",
+            "{controller}/{id}",
+            { Controller = "Home"; Id = RouteParameter.Optional }) |> ignore
+            )
